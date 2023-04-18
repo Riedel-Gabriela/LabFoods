1d89bee71f71009e38d490016153fc98510c29e9
Gabriela Riedel <gabrielariedel1@gmail.com>
2023-04-18 18:44:46 -0300

    chore(LabFood): css aplication

--- LabReceitas/src/components/organisms/ReceitaItem/ReceitaItem.jsx
+++ LabReceitas/src/components/organisms/ReceitaItem/ReceitaItem.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
+import './ReceitaItem.css'
+
 import { useCookBContext } from "../../../context/CookBContext";
 
 const ReceitaItem = ({ receita }) => {
@@ -11,20 +13,22 @@ const ReceitaItem = ({ receita }) => {
   };
 
   return (
-    <div className='receita-item'>
-      <h3>{receita.title}</h3>
-      {receita.restrictions ? (
-        <>
-          <h4>Não contém:</h4>
-          <p>{receita.restrictions}</p>
-        </>
-      ) : (
-        <h4>Esta receita contém glúten e lactose.</h4>
-      )}
+    <div className="item-container">
+      <div className='receita-item'>
+        <h3>{receita.title}</h3>
+        {receita.restrictions ? (
+          <>
+            <h4>Não contém:</h4>
+            <p>{receita.restrictions}</p>
+          </>
+        ) : (
+          <h4>Esta receita contém glúten e lactose.</h4>
+        )}
 
-      <button onClick={handleDelete}>Excluir</button>
-      <button onClick={() => navigate(`/details/${receita.id}`)}>Selecionar</button>
-      <button onClick={() => navigate(`/edit/${receita.id}`)}>Editar</button>
+        <button onClick={handleDelete}>Excluir</button>
+        <button onClick={() => navigate(`/details/${receita.id}`)}>Selecionar</button>
+        <button onClick={() => navigate(`/edit/${receita.id}`)}>Editar</button>
+      </div>
     </div>
   );
 };
